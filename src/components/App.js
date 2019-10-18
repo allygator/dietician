9aefd3b19ea9712a7b860aad2553f9f54b80e1fb
Alyssa <allygator95@gmail.com>
2019-10-18 14:32:41 -0400

    Added profile component and route, fixed login component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import '../App.css';
 import  {BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
+import Profile from './Profile';
 
 function App() {
     //Add additional routes here following the login route example
@@ -12,6 +13,7 @@ function App() {
               <Switch>
                   <Route path='/' exact component={Home}/>
                   <Route path='/login' component={Login}/>
+                  <Route path='/profile' component={Profile}/>
               </Switch>
           </Router>
   );
