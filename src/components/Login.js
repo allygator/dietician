9e3fed174e59cf5620464ec90d3da3343884ecc1
Kenny <kennygperez@gmail.com>
2019-10-24 11:07:58 -0400

    ran prettier on project

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import logo from '../logo.svg';
-import '../App.css';
+import React from "react";
+import "../App.css";
 
 function Login() {
-  return (
-    <div className="login">
-      Login page
-    </div>
-  );
+  return <div className="login">Login page</div>;
 }
 
 export default Login;
