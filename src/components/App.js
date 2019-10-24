038925d4073065419c5359b8fc03407c84bbc40e
Kenny <kennygperez@gmail.com>
2019-10-24 11:02:01 -0400

    DUFKC

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import logo from '../logo.svg';
-import '../App.css';
-import  {BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
-import Home from './Home';
-import Login from './Login';
+import React from "react";
+import "../App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import Calendar from "./Calendar/Calendar";
 
 function App() {
     //Add additional routes here following the login route example
   return (
-      <Router>
-              <Switch>
-                  <Route path='/' exact component={Home}/>
-                  <Route path='/login' component={Login}/>
-              </Switch>
-          </Router>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/calendar" component={Calendar} />
+      </Switch>
+    </Router>
   );
 }
 
