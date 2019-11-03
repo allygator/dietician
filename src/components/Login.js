edc8b680179e1a2a7636f8e159fe81338bab3c2c
csteph23 <cstephens@LAPTOP-TRIIVV2J.localdomain>
2019-11-02 20:44:04 -0400

    Added Textfields for user info and login button

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,50 @@
 import React from "react";
+import { useState } from "react";
 import "../App.css";
 
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
 function Login() {
-  return <div className="login">Login page</div>;
+  const [signinInputs, setSignin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChangeIn = name => event => {
+    setSignin({ ...signinInputs, [name]: event.target.value });
+  };
+
+  const handleClick = name => event => {};
+
+  return (
+    <div className="login_fields">
+      Dietician Login
+      <TextField
+        id="email"
+        defaultValue={signinInputs.email}
+        label="Email"
+        variant="outlined"
+        required={true}
+        onChange={handleChangeIn("email")}
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="password"
+        defaultValue={signinInputs.password}
+        label="Password"
+        variant="outlined"
+        required={true}
+        onChange={handleChangeIn("password")}
+        fullWidth
+        margin="normal"
+      />
+      <Button variant="contained" id="submitButton" onClick={handleClick()}>
+        Login
+      </Button>
+    </div>
+  );
 }
 
 export default Login;
